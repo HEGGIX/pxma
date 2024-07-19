55eda31a74acdb1db477f1c17ba03ff06c49c001
Gleb Ermakovich <glebermakovich@gmail.com>
2024-07-19 22:22:51 +0300

    change tabs

--- src/ui-components/tabs/tabs.tsx
+++ src/ui-components/tabs/tabs.tsx
@@ -7,33 +7,38 @@ import {ReactComponent as Settings} from "../../assets/settings.svg"
 import {Link } from "react-router-dom";
 import { useDispatch} from "react-redux";
 import { addTrendsMovies } from "../../store/trendsSlice"
+import { useState } from "react"
 
 
 export const Tabs = () => {
     const dispatch = useDispatch()
+    const [activeTab, setActiveTab] = useState(false)
+    const handleClick = () => {
+        setActiveTab(true)
+    }
     return(
         <>
         <div className = "tabs">
             <div className = "tabs-wrapper">
-                <div className = "tabs-container home">
+                <div className = {activeTab === true? "tabs-container active" : "tabs-container home"} onClick={() =>handleClick}>
                     <Link to = "/" className = "tabs-link">
                         <Home className = "tabs-logo"/>
                         <span className = "tabs-text">Home</span>
                     </Link>
                 </div>
-                <div className = "tabs-container">
+                <div className = {activeTab === true? "tabs-container active" : "tabs-container"} onClick={() => handleClick()}>
                     <Link to = "/trends" className = "tabs-link" onClick = {() =>{dispatch(addTrendsMovies())}}>
                         <Trends className = "tabs-logo"/>
                         <span className = "tabs-text">Trends</span>
                     </Link>
                 </div>
-                <div className = "tabs-container">
+                <div className = "tabs-container" onClick={() => toogleClass}>
                     <Link to = "/favorites" className = "tabs-link">
                         <Favorites className = "tabs-logo"/>
                         <span className = "tabs-text">Favorites</span>
                     </Link>
                 </div>
-                <div className = "tabs-container">
+                <div className = "tabs-container" onClick={() => toogleClass}>
                     <Link to = "/settings" className = "tabs-link">
                         <Settings className = "tabs-logo"/>
                         <span className = "tabs-text">Settings</span>
